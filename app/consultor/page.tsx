d9b1648e6f62225ea8336ad78e8c53141cf21331
Ignacio Bilbao <Bilbaonacho1@gmail.com>
2026-08-25 23:46:56 -0300

    fix: forzar renderizado dinamico en paginas con datos

--- app/consultor/page.tsx
+++ app/consultor/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import Link from "next/link";
 import { getProducts } from "@/lib/queries";
 
--- app/empresa/page.tsx
+++ app/empresa/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import Link from "next/link";
 import { getProducts } from "@/lib/queries";
 import type { ProductStatus } from "@/lib/types";
