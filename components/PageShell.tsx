e190c6aa54ce92a49fee8efeefb1b64f458f242f
Ignacio Bilbao <Bilbaonacho1@gmail.com>
2026-08-27 12:33:47 -0300

    feat: logo en mobile en pantallas internas

--- components/PageShell.tsx
+++ components/PageShell.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { SideBrand } from "./SideBrand";
 
 export function PageShell({
@@ -21,7 +22,21 @@ export function PageShell({
             />
             <div className="relative flex flex-1">
                 <SideBrand />
-                <div className="flex-1 flex flex-col min-w-0">{children}</div>
+                <div className="flex-1 flex flex-col min-w-0">
+                    <Link
+                        href="/"
+                        className="lg:hidden px-6 pt-6 hover:opacity-80 transition-opacity"
+                    >
+                        <Image
+                            src="/coralia.avif"
+                            alt="Coralia Environmental — ir al inicio"
+                            width={120}
+                            height={130}
+                            className="h-auto w-14"
+                        />
+                    </Link>
+                    {children}
+                </div>
             </div>
         </div>
     );
